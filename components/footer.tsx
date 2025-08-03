ca8a3a7fa92f096049f1e66ed7396aa8e3ba2614
Muhammad Yasir Malkani <muhammadyasirmalkani1@gmail.com>
2025-08-03 22:16:50 +0500

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ import { Github, Linkedin, Twitter } from "lucide-react"
 
 export default function Footer() {
   return (
-    <div className="w-full border-t bg-background">
+    <footer className="w-full border-t bg-background">
       <div className="container flex flex-col items-center gap-4 py-10 md:h-24 md:flex-row md:py-0">
         <div className="flex flex-1 items-center justify-center md:justify-start">
           <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
